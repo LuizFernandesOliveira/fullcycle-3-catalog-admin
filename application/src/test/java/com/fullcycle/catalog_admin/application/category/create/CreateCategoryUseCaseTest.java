827c87d12132329311cc3e1ba3fcfb2c735d248f
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2024-11-03 10:51:50 -0300

    feat: add test

--- application/src/test/java/com/fullcycle/catalog_admin/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcycle/catalog_admin/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,9 +1,11 @@
 package com.fullcycle.catalog_admin.application.category.create;
 
 import com.fullcycle.catalog_admin.domain.category.CategoryGateway;
+import com.fullcycle.catalog_admin.domain.exception.DomainException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -12,11 +14,15 @@ import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CreateCategoryUseCaseTest {
+  @InjectMocks
+  private DefaultCreateCategoryUseCase useCase;
+
   @Mock
   private CategoryGateway categoryGateway;
 
@@ -30,8 +36,8 @@ class CreateCategoryUseCaseTest {
     when(categoryGateway.create(any()))
         .thenAnswer(AdditionalAnswers.returnsFirstArg());
 
-    final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
     final var actualOutput = useCase.execute(command);
+
     assertNotNull(actualOutput);
     assertNotNull(actualOutput.id());
     verify(categoryGateway).create(argThat(category -> {
@@ -42,5 +48,18 @@ class CreateCategoryUseCaseTest {
           Objects.nonNull(category.getUpdatedAt()) &&
           Objects.isNull(category.getDeletedAt());
     }));
+  }
+
+  @Test
+  void givenAInvalidName_whenCallsCreateCategory_thenShouldDomainException() {
+    final var expectedName = "";
+    final var expectedDescription = "Category Description";
+    final var expectedIsActive = true;
+
+    final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+    assertThrows(DomainException.class, () -> useCase.execute(command));
+
+    verify(categoryGateway, never()).create(any());
   }
 }
